96abbcf52a7bc20119c5e966e611327f8dfb71e9
klucly <61903562+klucly@users.noreply.github.com>
2020-07-25 19:10:08 +0300

    Add files via upload

--- PyEngine2.py
+++ PyEngine2.py
@@ -88,7 +88,6 @@ class Physics():
         def __init__(self, space, obj, body_type = DYNAMIC, mass = 1, elasticity = .9):
 
             self.__sprite__ = obj
-            # self.coords1 = [self.__sprite__.coords[0], self.__sprite__.coords[1]], [self.__sprite__.coords[2], self.__sprite__.coords[3]]
             self.coords = self.__sprite__.coords
             self.body_type = body_type
             self.__sprite__.RigitBody = self
@@ -108,12 +107,49 @@ class Physics():
             self.space.__objectList__.append(self)
 
             self.space.__space__.add(self.body, self.poly)
+            
+    class PolygonRigitBody():
+
+        def __init__(self, space, obj, body_type = DYNAMIC, mass = 1, elasticity = .9):
+
+            self.__sprite__ = obj
+            self.coords = []
+            for i in range(len(self.__sprite__.coords)-1):
+                if i%2 == 0: self.coords.append([self.__sprite__.coords[i], self.__sprite__.coords[i+1]])
+                
+            self.body_type = body_type
+            self.__sprite__.RigitBody = self
+            self.radius = self.__sprite__.border
+            self.angle = 0
+            self.mass = mass
+            self.moment = basephysicsengine.moment_for_circle(self.mass, 0, self.radius)
+            self.elasticity = elasticity
+
+            self.body = basephysicsengine.Body(self.mass, self.moment, self.body_type)
+            self.body.position = 0, 0
+            self.body.angle = math.radians(self.angle)
+
+            self.poly = basephysicsengine.Poly(self.body, self.coords, None, self.radius)
+            self.poly.elasticity = self.elasticity
+            self.space = space
+            self.space.__objectList__.append(self)
+
+            self.space.__space__.add(self.body, self.poly)
 
 
     def updatePhysics(self):
         
         for obj in self.__objectList__:
-            obj.coords = obj.body.position
+            if obj.__class__ == Physics.RectRigitBody or obj.__class__ == Physics.CircleRigitBody:
+                obj.coords = obj.body.position
+            elif obj.__class__ == Physics.SegmentRigitBody:
+                obj.coords = [obj.poly.a[0], obj.poly.a[1], obj.poly.b[0], obj.poly.b[1]]
+            elif obj.__class__ == Physics.PolygonRigitBody:
+                vertices = obj.poly.get_vertices()
+                for i in range(vertices.__len__()):
+                    vertices[i] = [vertices[i][0]+obj.body.position[0], vertices[i][1]+obj.body.position[1]]
+                obj.coords = vertices
+                
             obj.angle = obj.body.angle
 
             if obj.__class__ == self.RectRigitBody:
@@ -123,8 +159,8 @@ class Physics():
             elif obj.__class__ == self.CircleRigitBody:
                 obj.__sprite__.set_position([obj.coords[0], obj.coords[1]])
 
-            elif obj.__class__ == self.SegmentRigitBody:
-                obj.__sprite__.set_position(obj.coords)
+            elif obj.__class__ == self.SegmentRigitBody or obj.__class__ == self.PolygonRigitBody:
+                obj.__sprite__.value_change(coords = obj.coords)
                 
 
         self.__space__.step(1/self.fps)
@@ -2045,6 +2081,7 @@ class Polygon(DefaultCanvasObject):
     def __init__(self, win, coords = [10, 20, 50, 100, 150, 30, 200, 200], bg = "#ffffff", border = 1, outline = "#000000"):
 
         self.coords = coords
+        self.angle = 0
         self.bg = bg
         self.border = border
         self.outline = outline
@@ -2064,7 +2101,7 @@ class Polygon(DefaultCanvasObject):
         if outline == DEFAULT:
             outline = self.outline
 
-        self.__init__(coords, bg, border, outline)
+        self.__init__(self.__win__, coords, bg, border, outline)
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 
